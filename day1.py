d4a04f62de8971f33b59034b6cef083890ed066c
ajsth <aj.sth.2022@gmail.com>
2025-02-24 11:40:17 +1100

    day2-1

--- day1.py
+++ day1.py
@@ -1,8 +1,9 @@
-import typing
-
 with open("input1") as file:
-    input = file.readlines()
+    input = file.read().splitlines()
 
+# part 1
+# time complexity: O(n)
+# space complexity: O(n)
 list1 = []
 list2 = []
 
@@ -14,11 +15,30 @@ for line in input:
 list1.sort()
 list2.sort()
 
-res: list[int] = []
+total_distance = []
 
 for n, i in enumerate(list1):
     # print(n, " ", i)
-    res.append(abs(int(list1[n]) - int(list2[n])))
+    total_distance.append(abs(int(list1[n]) - int(list2[n])))
 
-print(sum(res))
+print("total distance: ", sum(total_distance))
+
+# part 2
+dict1 = dict()
+dict2 = dict()
+for i in range(len(input)):
+    num1 = int(list1[i])
+    num2 = int(list2[i])
+    dict1[num1] = dict1.get(num1, 0) + 1
+    dict2[num2] = dict2.get(num2, 0) + 1
+
+similarity_score = 0
+for i in dict1.keys():
+    similarity_score += dict1[i] * dict2.get(i, 0)
+
+for i in list1:
+    print(i)
+
+print(similarity_score)
+
 
